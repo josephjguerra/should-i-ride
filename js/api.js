236762126ae93c4623f7803389e041166f92602f
josephjguerra <joseph.j.guerra@gmail.com>
2017-06-18 11:04:00 -0400

    cleanup of 3 requests

---
+++ js/api.js
@@ -0,0 +1,126 @@
+// api key from js/secrets.js
+var wundergroundAPIKey = WUNDERGROUNDAPIKEY;
+
+// dev mode
+var devMode = true; // set to false to pull from api. Make sure you have a key
+var zipCode = '29708';
+
+if (devMode) {
+  // use local copies of json
+  var wundergroundConditionsURL = 'js/dev/conditions.json'
+  var wundergroundForecastURL   = 'js/dev/forecast.json'
+  var wundergroundAstronomyURL  = 'js/dev/astronomy.json'
+  console.log("dev mode active");
+} else {
+  // use wunderground weather api
+  var wundergroundConditionsURL = 'http://api.wunderground.com/api/' + wundergroundAPIKey + '/geolookup/conditions/q/' + zipCode + '.json';
+  var wundergroundForecastURL   = 'http://api.wunderground.com/api/' + wundergroundAPIKey + '/geolookup/forecast/q/' + zipCode + '.json';
+  var wundergroundAstronomyURL  = 'http://api.wunderground.com/api/' + wundergroundAPIKey + '/geolookup/astronomy/q/' + zipCode + '.json';
+  console.log("dev mode inactive");
+}
+
+////////////////////////////////////////////////////////////
+////////////////////// Conditions
+////////////////////////////////////////////////////////////
+
+var getConditionsJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
+
+getConditionsJSON(wundergroundConditionsURL).then(
+  function(data) {
+    alert('JSON result: ' + data.location.city); // debug
+    document.getElementById("conditions-city-location").textContent = data.location.city;
+    document.getElementById("day-current-temp").textContent = Math.round(data.current_observation.temp_f);
+    document.getElementById("wind-speed").textContent = Math.round(data.current_observation.wind_mph);
+    document.getElementById("wind-gust").textContent  = Math.round(data.current_observation.wind_gust_mph);
+  },
+  function(status) {
+    //error detection....
+    console.log('Something went wrong.');
+    alert('Something went wrong.');
+  }
+);
+
+////////////////////////////////////////////////////////////
+////////////////////// Forecast
+////////////////////////////////////////////////////////////
+
+var getForecastJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
+
+getForecastJSON(wundergroundForecastURL).then(
+  function(data) {
+    alert('JSON result: ' + data.forecast.simpleforecast.forecastday[0].high.fahrenheit); // debug
+    alert('JSON result: ' + data.forecast.simpleforecast.forecastday[0].low.fahrenheit); // debug
+    document.getElementById("day-high").textContent   = data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+    document.getElementById("day-low").textContent    = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+  },
+  function(status) {
+    //error detection....
+    console.log('Something went wrong.');
+    alert('Something went wrong.');
+  }
+);
+
+////////////////////////////////////////////////////////////
+////////////////////// Astronomy
+////////////////////////////////////////////////////////////
+
+var getAstronomyJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
+
+getAstronomyJSON(wundergroundAstronomyURL).then(
+  function(data) {
+    // alert('JSON result: ' + data.sun_phase.sunrise.hour); // debug
+    document.getElementById("sunrise").textContent = data.sun_phase.sunrise.hour + ":" +  data.sun_phase.sunrise.minute + " am";
+    document.getElementById("sunset").textContent  = data.sun_phase.sunset.hour - 12 + ":" +  data.sun_phase.sunset.minute + " pm";
+
+  },
+  function(status) {
+    //error detection....
+    console.log('Something went wrong.');
+    alert('Something went wrong.');
+  }
+);
--- js/app.js
+++ js/app.js
@@ -1,124 +1,38 @@
-// api key from js/secrets.js
-var wundergroundAPIKey = WUNDERGROUNDAPIKEY;
-
-// dev mode
-var devMode = true;
-
-if (devMode) {
-  // use local copies of json
-  var wundergroundConditionsURL = 'js/dev/conditions.json'
-  var wundergroundAstronomyURL  = 'js/dev/astronomy.json'
-  console.log("dev mode active");
-} else {
-  // use wunderground weather api
-  var wundergroundConditionsURL = 'http://api.wunderground.com/api/' + wundergroundAPIKey + '/geolookup/conditions/q/29708.json';
-  var wundergroundAstronomyURL  = 'http://api.wunderground.com/api/' + wundergroundAPIKey + '/geolookup/astronomy/q/29708.json';
-  console.log("dev mode inactive");
-}
-
-var getConditionsJSON = function(url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-    xhr.send();
-  });
-};
-
-getConditionsJSON(wundergroundConditionsURL).then(
-  function(data) {
-    alert('JSON result: ' + data.current_observation.temp_f); // debug
-    document.getElementById("day-current-temp").textContent = data.current_observation.temp_f;
-    document.getElementById("wind-speed").textContent = data.current_observation.wind_mph;
-    document.getElementById("wind-gust").textContent  = data.current_observation.wind_gust_mph;
-  },
-  function(status) {
-    //error detection....
-    console.log('Something went wrong.');
-    alert('Something went wrong.');
-  }
-);
-
-var getAstronomyJSON = function(url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-    xhr.send();
-  });
-};
-
-getAstronomyJSON(wundergroundAstronomyURL).then(
-  function(data) {
-    alert('JSON result: ' + data.sun_phase.sunrise.hour); // debug
-    document.getElementById("sunrise").textContent = data.sun_phase.sunrise.hour + ":" +  data.sun_phase.sunrise.minute + " am";
-    document.getElementById("sunset").textContent  = data.sun_phase.sunset.hour - 12 + ":" +  data.sun_phase.sunset.minute + " pm";
-
-  },
-  function(status) {
-    //error detection....
-    console.log('Something went wrong.');
-    alert('Something went wrong.');
-  }
-);
+// user preferences
+var myMaxTemp            = 95;
+var myMinTemp            = 50;
+var myMaxPrecip          = 25;
+var myMaxWinds           = 15;
+var willIRideInRain      = true;
+var willIRideAtNight     = false;
 
 // top card
-var myMaxTemp        = 95;
-var myMinTemp        = 50;
-var myMaxPrecip      = 25;
-var myMaxWinds       = 15;
-var willIRideInRain  = true;
-var willIRideAtNight = false;
-
-var currentCondition   = "Sunny";
-
-var observedTemp       = 80;
-var observedHighTemp   = 90;
-var observedLowTemp    = 70;
-
-var currentChancePrecip = 11;
-
-var observedWindSpeed     = 5;
-var observedGust          = 10;
-
-var sunrise = "6:11am";
-var sunset  = "8:11pm";
+var currentCondition     = "Sunny";
+var observedTemp         = 80;
+var observedHighTemp     = 90;
+var observedLowTemp      = 70;
+var currentChancePrecip  = 11;
+var observedWindSpeed    = 5;
+var observedGust         = 10;
+var sunrise              = "6:11am";
+var sunset               = "8:11pm";
 
 // morning afternoon evening cards
-var morningCondition   = "Sunny";
-var afternoonCondition = "Partly Cloudy";
-var eveningCondition   = "Chance of Rain";
+var morningCondition     = "Sunny";
+var afternoonCondition   = "Partly Cloudy";
+var eveningCondition     = "Chance of Rain";
 
-// populating top card
+// populating top card with defaults
 document.getElementById("day-current-temp").textContent  = observedTemp;
 document.getElementById("day-high").textContent          = observedHighTemp;
 document.getElementById("day-low").textContent           = observedLowTemp;
-
 document.getElementById("chance-of-precip").textContent  = currentChancePrecip;
+document.getElementById("sunrise").textContent           = sunrise;
+document.getElementById("sunset").textContent            = sunset;
+document.getElementById("wind-speed").textContent        = observedWindSpeed;
+document.getElementById("wind-gust").textContent         = observedGust;
 
-document.getElementById("sunrise").textContent = sunrise;
-document.getElementById("sunset").textContent  = sunset;
-
-document.getElementById("wind-speed").textContent = observedWindSpeed;
-document.getElementById("wind-gust").textContent  = observedGust;
-
-//setting icon - need to add all available
+//setting icon - TODO: add all available
 function setIconBasedOnCondition(condition, id) {
   if (condition == "Sunny") {
     document.getElementById(id).src = "img/conditions/day/clear.svg";
@@ -131,7 +45,7 @@ function setIconBasedOnCondition(condition, id) {
   }
 }
 
-// decision algorithm
+// decision algorithm - not currently working with json request data
 function yesDecision() {
   document.getElementById("decision").textContent = "Yes";
   document.getElementById("decision-image").src   = "img/emotions/thumbup.svg";
@@ -147,6 +61,7 @@ function applyNoDecisionColor(id) {
   document.getElementById(id).classList.add("no-ride-table");
 }
 
+// actual logic for ride or no tide
 function calculateRideOrNoRide() {
   if (
     myMaxTemp > observedTemp &&

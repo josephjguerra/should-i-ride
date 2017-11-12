1efe2d5b7eab0df53ce436a11a129b5dfe55c00d
josephjguerra <joseph.j.guerra@gmail.com>
2017-11-12 16:25:41 -0500

    hooks up local weather metrics

--- js/app.js
+++ js/app.js
@@ -15,23 +15,22 @@ var myMaxWinds           = 15;
 var willIRideInRain      = true;
 var willIRideAtNight     = false;
 
-// top card current conditions placeholder variables
+// placeholder variables
 var currentCondition     = "Sunny";
-var observedTemp         = 88;
-var feelsLikeTemp        = 90;
-var observedHighTemp     = 99;
-var observedLowTemp      = 77;
+var observedTemp         = 77;
+var observedHighTemp     = 88;
+var observedLowTemp      = 66;
 var currentChancePrecip  = 11;
-var observedWindSpeed    = 11;
-var observedGust         = 22;
+var observedWindSpeed    = 22;
+var observedGust         = 33;
 var sunrise              = "1:11am";
 var sunset               = "1:11pm";
 var observedTime         = "Last Updated on June 18, 10:00"
 
-// morning afternoon evening cards placeholder variables
-var morningCondition     = "Sunny";
-var afternoonCondition   = "Partly Cloudy";
-var eveningCondition     = "Chance of Rain";
+// // morning afternoon evening cards placeholder variables
+// var morningCondition     = "Sunny";
+// var afternoonCondition   = "Partly Cloudy";
+// var eveningCondition     = "Chance of Rain";
 
 // // populating top card with defaults
 // document.getElementById("day-current-temp").textContent        = observedTemp;
@@ -48,23 +47,23 @@ var eveningCondition     = "Chance of Rain";
 // document.getElementById("today-observed-time").textContent = observedTime;
 //
 // //setting icon - TODO: add all available ---OR--- use images from json reponse
-// function setIconBasedOnCondition(condition, id) {
-//   if (condition == "Sunny") {
-//     document.getElementById(id).src = "img/conditions/day/clear.svg";
-//   } else if (condition == "Partly Cloudy") {
-//     document.getElementById(id).src = "img/conditions/day/partlycloudy.svg";
-//   } else if (condition == "Mostly Cloudy") {
-//     document.getElementById(id).src = "img/conditions/day/mostlycloudy.svg";
-//   } else if (condition == "Chance of Rain") {
-//     document.getElementById(id).src = "img/conditions/day/chancerain.svg";
-//   } else if (condition == "Chance of a Thunderstorm") {
-//     document.getElementById(id).src = "img/conditions/day/chancetstorms.svg";
-//   } else if (condition == "Cloudy") {
-//     document.getElementById(id).src = "img/conditions/day/cloudy.svg";
-//   } else if (condition == "Snow") {
-//     document.getElementById(id).src = "img/conditions/day/snow.svg";
-//   }
-// }
+function setIconBasedOnCondition(condition, id) {
+  if (condition == "Sunny") {
+    document.getElementById(id).src = "img/conditions/day/clear.svg";
+  } else if (condition == "Partly Cloudy") {
+    document.getElementById(id).src = "img/conditions/day/partlycloudy.svg";
+  } else if (condition == "Mostly Cloudy") {
+    document.getElementById(id).src = "img/conditions/day/mostlycloudy.svg";
+  } else if (condition == "Chance of Rain") {
+    document.getElementById(id).src = "img/conditions/day/chancerain.svg";
+  } else if (condition == "Chance of a Thunderstorm") {
+    document.getElementById(id).src = "img/conditions/day/chancetstorms.svg";
+  } else if (condition == "Cloudy") {
+    document.getElementById(id).src = "img/conditions/day/cloudy.svg";
+  } else if (condition == "Snow") {
+    document.getElementById(id).src = "img/conditions/day/snow.svg";
+  }
+}
 //
 // // decision algorithm - not currently working with json request data
 // function yesDecisionOnCurrentConditionsCard() {
@@ -97,11 +96,13 @@ var eveningCondition     = "Chance of Rain";
 //
 // if (devMode) {
 //   // use local copies of json to limit api call limits and use familiar data
+  var wundergroundConditionsURL      = 'js/dev/clt-conditions.json'
+  var wundergroundForecast10dayURL   = 'js/dev/clt-forecast10day.json'
 //   var wundergroundConditionsURL      = 'js/dev/conditions.json'
 //   var wundergroundForecast10dayURL   = 'js/dev/forecast10day.json'
 //   var wundergroundHourlyURL          = 'js/dev/hourly.json'
 //   var wundergroundAstronomyURL       = 'js/dev/astronomy.json'
-//   console.log("dev mode active - using local data");
+  console.log("dev mode active - using local data");
 // } else {
 //   // use wunderground weather api for LIVE data
 //   var wundergroundAPIKey             = WUNDERGROUNDAPIKEY;
@@ -112,32 +113,44 @@ var eveningCondition     = "Chance of Rain";
 //   console.log("dev mode inactive. You're LIVE");
 // }
 //
-// var getWundergroundJSON = function(url) {
-//   return new Promise(function(resolve, reject) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('get', url, true);
-//     xhr.responseType = 'json';
-//     xhr.onload = function() {
-//       var status = xhr.status;
-//       if (status == 200) {
-//         resolve(xhr.response);
-//       } else {
-//         reject(status);
-//       }
-//     };
-//     xhr.send();
-//   });
-// };
+var getWundergroundJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
 //
 // // main function to get data and process
 // // how can I refactor  this to make dry?
 // // how can I pull out logic into individual functions?
-// async function getWeatherAndCompute() {
-//   var conditionsData    = await getWundergroundJSON(wundergroundConditionsURL);
-//   var forecast10dayData = await getWundergroundJSON(wundergroundForecast10dayURL);
+async function getWeatherAndCompute() {
+  var conditionsData    = await getWundergroundJSON(wundergroundConditionsURL);
+  var forecast10dayData = await getWundergroundJSON(wundergroundForecast10dayURL);
 //   var hourlyData        = await getWundergroundJSON(wundergroundHourlyURL);
 //   var astronomyData     = await getWundergroundJSON(wundergroundAstronomyURL);
-//   console.log(conditionsData);    // debug
+  console.log(conditionsData);    // debug
+  document.getElementById("temp").textContent = Math.round(conditionsData.current_observation.temp_f);
+  document.getElementById("pop").textContent = forecast10dayData.forecast.simpleforecast.forecastday[0].pop;
+  document.getElementById("high-temp").textContent = forecast10dayData.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+  document.getElementById("low-temp").textContent = forecast10dayData.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+  document.getElementById("observed-time").textContent = conditionsData.current_observation.observation_time;
+
+
+  var conditions = forecast10dayData.forecast.simpleforecast.forecastday[0].conditions
+  document.getElementById("condition").textContent = conditions;
+  setIconBasedOnCondition(conditions, "condition-icon");
+}
+getWeatherAndCompute();
 //   console.log(forecast10dayData); // debug
 //   console.log(astronomyData);     // debug
 //
